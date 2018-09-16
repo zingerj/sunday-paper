53cda54516baaf34d2a1e4e11f356ed8362f2cd5
Jonathan Zinger <jzinger1@babson.edu>
2018-09-16 07:54:00 -0400

    🔮 Add error case

--- frontend/src/components/Online.js
+++ frontend/src/components/Online.js
@@ -7,34 +7,58 @@ import {
     IconAppear
 } from './styled'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
 class Online extends Component {
     render () {
-        const { loading } = this.props
+        const { loading, error } = this.props
+        let content
+
+        if (loading) {
+            content = (
+                <LoadingContainer>
+                    <LoadingText>
+                        Extra! Extra!<br />
+                        Printing your Sunday Paper...
+                        </LoadingText>
+                    <LoadingImage><span role="img" aria-label="newspaper">📰</span></LoadingImage>
+                </LoadingContainer>
+            )
+        } else if (error) {
+            content = (
+                <LoadingContainer>
+                    <LoadingText>
+                        Whoops! Looks like something went wrong. Check your link and try again.
+                    </LoadingText>
+                    <IconAppear>
+                        <FontAwesomeIcon
+                            icon={faTimesCircle}
+                            size='5x'
+                            color='red'
+                        />
+                    </IconAppear>
+                </LoadingContainer>
+            )
+        } else {
+            content = (
+                <LoadingContainer>
+                    <LoadingText>
+                        You're all set! To enjoy your paper distraction free, please disable your internet.
+                        </LoadingText>
+                    <IconAppear>
+                        <FontAwesomeIcon
+                            icon={faCheckCircle}
+                            size='5x'
+                            color='green'
+                        />
+                    </IconAppear>
+                </LoadingContainer>
+            )
+        }
 
         return (
             <OnlineContainer>
-                {loading
-                    ? <LoadingContainer>
-                        <LoadingText>
-                            Extra! Extra!<br />
-                            Printing your Sunday Paper...
-                        </LoadingText>
-                        <LoadingImage><span role="img" aria-label="newspaper">📰</span></LoadingImage>
-                    </LoadingContainer>
-                    : <LoadingContainer>
-                        <LoadingText>
-                            You're all set! To enjoy your paper distraction free, please disable your internet.
-                        </LoadingText>
-                        <IconAppear>
-                            <FontAwesomeIcon
-                                icon={faCheckCircle}
-                                size='5x'
-                                color='green'
-                            />
-                        </IconAppear>
-                    </LoadingContainer>}
+                {content}
             </OnlineContainer>
         )
     }
