866b263b275d37cd80dc7a417a6b79a38c6d9e3a
Jonathan Zinger <jzinger1@babson.edu>
2018-09-16 07:02:07 -0400

    ☁️ Add cloud functions
    * Update .gitignore & add firebase function
    
    * Add sendEmail & update firebase query
    
    * Update articles to same paper

--- extension/credentials.js
+++ extension/credentials.js
@@ -10,6 +10,10 @@ var config = {
   messagingSenderId: "298992067325"
 };
 firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
+const settings = { /* your settings... */ timestampsInSnapshots: true };
+firestore.settings(settings);
 /**
  * initApp handles setting up the Firebase context and registering
  * callbacks for the auth status.
@@ -29,37 +33,100 @@ function initApp() {
   // [START authstatelistener]
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      document.getElementById('add-site').disabled = true;
       // // User is signed in.
       // Enable the add site button
-      document.getElementById('add-site').disabled = false;
-      document.getElementById('add-site').hidden = false;
+      document.getElementById("add-site").disabled = false;
+      document.getElementById("add-site").hidden = false;
       // // [START_EXCLUDE]
-      document.getElementById('quickstart-button').textContent = 'Sign out';
+      document.getElementById("quickstart-button").textContent = "Sign out";
       // [END_EXCLUDE]
     } else {
       // Let's try to get a Google auth token programmatically.
-      document.getElementById('add-site').disabled = true;
-      // Disable add-site 
-      document.getElementById('add-site').disabled = true;
-      document.getElementById('add-site').hidden = true;
+      document.getElementById("add-site").disabled = true;
+      // Disable add-site
+      document.getElementById("add-site").disabled = true;
+      document.getElementById("add-site").hidden = true;
       // [START_EXCLUDE]
-      document.getElementById('quickstart-button').textContent = 'Sign-in with Google';
+      document.getElementById("quickstart-button").textContent =
+        "Sign-in with Google";
       // [END_EXCLUDE]
     }
   });
   // [END authstatelistener]
-  document.getElementById('quickstart-button').addEventListener('click', startSignIn, false);
+  document
+    .getElementById("quickstart-button")
+    .addEventListener("click", startSignIn, false);
   // document.getElementById('add-site').addEventListener('click', scrapeMedium, false);
-  document.getElementById('add-site').addEventListener('click', scrapeMedium, false);
+  document
+    .getElementById("add-site")
+    .addEventListener("click", scrapeMedium, false);
 }
 
 function scrapeMedium() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     var tab = tabs[0];
     var tabID = tab.id;
-    // Gets the dom data as a json with title, author, content, date posted, and link to article 
-    chrome.tabs.sendMessage(tabID, {text: 'getDom'}, (x) => {console.log(x)});
+    // Gets the dom data as a json with title, author, content, date posted, and link to article
+    chrome.tabs.sendMessage(tabID, { text: "getDom" }, x => {
+      var user = firebase.auth().currentUser;
+      console.log(user.uid);
+      console.log(user.displayName);
+
+      var querySize = 0;
+
+      firestore
+        .collection("users")
+        .doc(user.uid)
+        .collection("papers")
+        .doc("LOW4ylqnZpNoCSXJiVYq")
+        .collection("articles")
+        .doc()
+        .set({
+          title: x.title,
+          author: x.author,
+          date: x.date,
+          content: x.content,
+          link: x.link,
+          timestamp: x.timestamp
+        });
+
+      // firestore
+      //   .collection("users")
+      //   .doc(user.uid)
+      //   .collection("papers")
+      //   .get()
+      //   .then(query => {
+      //     var papers = [];
+      //     querySize = query.size;
+      //     console.log(querySize);
+      //     console.log(query);
+
+      //     query.forEach(doc => {
+      //       papers.push(doc);
+      //     });
+
+      //     console.log(papers[0]);
+
+      //     if (querySize > 0) {
+      //       firestore
+      //         .collection("users")
+      //         .doc(user.uid)
+      //         .collection("papers")
+      //         .doc(papers[0].id)
+      //         .collection("articles")
+      //         .doc()
+      //         .set({
+      //           title: x[title],
+      //           author: x[author],
+      //           date: x[date],
+      //           content: x[content],
+      //           link: x[link],
+      //           timestamp: x[timestamp]
+      //         });
+      //     } else {
+      //     }
+      //   });
+    });
   });
 }
 
@@ -69,28 +136,31 @@ function scrapeMedium() {
  */
 function startAuth(interactive) {
   // Request an OAuth token from the Chrome Identity API.
-  chrome.identity.getAuthToken({interactive: !!interactive}, function(token) {
+  chrome.identity.getAuthToken({ interactive: !!interactive }, function(token) {
     if (chrome.runtime.lastError && !interactive) {
-      console.log('It was not possible to get a token programmatically.');
-    } else if(chrome.runtime.lastError) {
+      console.log("It was not possible to get a token programmatically.");
+    } else if (chrome.runtime.lastError) {
       console.error(chrome.runtime.lastError);
     } else if (token) {
       // Authorize Firebase with the OAuth Access Token.
       var credential = firebase.auth.GoogleAuthProvider.credential(null, token);
-      firebase.auth().signInAndRetrieveDataWithCredential(credential).catch(function(error) {
-        // The OAuth token might have been invalidated. Lets' remove it from cache.
-        if (error.code === 'auth/invalid-credential') {
-          chrome.identity.removeCachedAuthToken({token: token}, function() {
-            startAuth(interactive);
-          });
-        }
-      });
+      firebase
+        .auth()
+        .signInAndRetrieveDataWithCredential(credential)
+        .catch(function(error) {
+          // The OAuth token might have been invalidated. Lets' remove it from cache.
+          if (error.code === "auth/invalid-credential") {
+            chrome.identity.removeCachedAuthToken({ token: token }, function() {
+              startAuth(interactive);
+            });
+          }
+        });
     } else {
-      console.error('The OAuth Token was null');
+      console.error("The OAuth Token was null");
     }
   });
-  document.getElementById('add-site').disabled = false;
-  chrome.contextMenus.create({"title":"Save for Sunday"});
+  document.getElementById("add-site").disabled = false;
+  chrome.contextMenus.create({ title: "Save for Sunday" });
   chrome.contextMenus.onClicked(scrapeMedium());
 }
 
@@ -107,4 +177,4 @@ function startSignIn() {
 
 window.onload = function() {
   initApp();
-}
+};
